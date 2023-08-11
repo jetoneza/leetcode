fa00a6d0532168752b40a7eb861e2f6054638d26
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-11 19:44:02 +0800

    Implement rust solution of valid_anagram

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -1,1 +1,2 @@
 pub mod s001_contains_duplicate;
+pub mod s002_valid_anagram;
---
+++ rust/src/solutions/s002_valid_anagram.rs
@@ -0,0 +1,37 @@
+#![allow(dead_code, unused_variables)]
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let first = sort(s);
+        let second = sort(t);
+
+        first == second
+    }
+}
+
+fn sort(s: String) -> String {
+    let mut word: Vec<char> = s.chars().collect();
+
+    word.sort();
+
+    word.into_iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::s002_valid_anagram::Solution;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            Solution::is_anagram("anagram".to_string(), "nagaram".to_string()),
+            true
+        );
+        assert_eq!(
+            Solution::is_anagram("rat".to_string(), "car".to_string()),
+            false
+        );
+    }
+}
