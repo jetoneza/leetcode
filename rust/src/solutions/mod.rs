9f9eee9bf6c339f8e0d101a9bb92bc3db25e24d0
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-13 20:28:20 +0800

    Implement rust solution for longest_consecutive_sequence

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -5,3 +5,4 @@ pub mod s004_group_anagrams;
 pub mod s005_top_k_frequent_elements;
 pub mod s006_product_of_array_except_self;
 pub mod s007_valid_sudoku;
+pub mod s008_longest_consecutive_sequence;
---
+++ rust/src/solutions/s008_longest_consecutive_sequence.rs
@@ -0,0 +1,61 @@
+#![allow(dead_code, unused_variables)]
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+
+        let mut sorted = nums.clone();
+        sorted.sort();
+
+        let mut check = sorted[0]; // Current number to be checked
+        let mut len = 1; // Current length of series
+        let mut longest = 0; // Longest length of all the checked series
+
+        for i in 0..sorted.len() {
+            let num = sorted[i];
+
+            if num == check {
+                continue;
+            }
+
+            if num == check + 1 {
+                len += 1;
+                check = num;
+                continue;
+            }
+
+            if longest < len {
+                longest = len
+            }
+
+            check = num;
+            len = 1;
+        }
+
+        if longest < len {
+            longest = len
+        }
+
+        longest
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::s008_longest_consecutive_sequence::Solution;
+
+    #[test]
+    fn it_works() {
+        let input_1 = Vec::from([100, 4, 200, 1, 3, 2]);
+        let input_2 = Vec::from([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]);
+        let input_3 = Vec::from([]);
+
+        assert_eq!(Solution::longest_consecutive(input_1), 4);
+        assert_eq!(Solution::longest_consecutive(input_2), 9);
+        assert_eq!(Solution::longest_consecutive(input_3), 0);
+    }
+}
