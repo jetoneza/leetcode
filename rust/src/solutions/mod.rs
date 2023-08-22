eaf8b8f2e66d6ff70e9c094d24ede08f7b990c64
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-23 00:04:24 +0800

    solution(rust): s025_invert_binary_tree

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -33,3 +33,6 @@ pub mod s022_max_profit;
 // Linked list
 pub mod s023_reverse_linked_list;
 pub mod s024_merged_two_sorted_lists;
+
+// Trees
+pub mod s026_invert_binary_tree;
---
+++ rust/src/solutions/s026_invert_binary_tree.rs
@@ -0,0 +1,125 @@
+#![allow(dead_code, unused_variables)]
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Solution::swap(&root);
+
+        root
+    }
+
+    fn swap(curr: &Option<Rc<RefCell<TreeNode>>>) {
+        if let Some(node) = curr {
+            let left = node.borrow().left.clone();
+            let right = node.borrow().right.clone();
+
+            node.borrow_mut().right = left;
+            node.borrow_mut().left = right;
+
+            Solution::swap(&node.borrow().left);
+            Solution::swap(&node.borrow().right);
+        }
+    }
+}
+
+fn create_tree(input: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    let mut root = None;
+
+    for &i in input.iter() {
+        root = insert_to_tree(root, i);
+    }
+
+    root
+}
+
+fn insert_to_tree(
+    mut root: Option<Rc<RefCell<TreeNode>>>,
+    val: i32,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    let node = Some(Rc::new(RefCell::new(TreeNode {
+        val,
+        left: None,
+        right: None,
+    })));
+
+    if root.is_none() {
+        root = node;
+        return root;
+    }
+
+    let mut q = vec![root.to_owned()];
+
+    while !q.is_empty() {
+        let curr = q.remove(0);
+
+        if let Some(n) = curr {
+            if n.borrow().left.is_none() {
+                n.borrow_mut().left = node;
+                return root;
+            } else if n.borrow().right.is_none() {
+                n.borrow_mut().right = node;
+                return root;
+            }
+
+            q.push(n.borrow().left.clone());
+            q.push(n.borrow().right.clone());
+        }
+    }
+
+    root
+}
+
+fn compare(a: Option<Rc<RefCell<TreeNode>>>, b: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match (a, b) {
+        (None, None) => true,
+        (Some(_), None) | (None, Some(_)) => false,
+        (Some(a), Some(b)) => {
+            if a.borrow().val != b.borrow().val {
+                return false;
+            }
+
+            return compare(a.borrow().left.to_owned(), b.borrow().left.to_owned())
+                && compare(a.borrow().right.to_owned(), b.borrow().right.to_owned());
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::s026_invert_binary_tree::compare;
+    use crate::solutions::s026_invert_binary_tree::create_tree;
+    use crate::solutions::s026_invert_binary_tree::Solution;
+
+    #[test]
+    fn it_works() {
+        let input = vec![4, 2, 7, 1, 3, 6, 9];
+        let reversed_input = vec![4, 7, 2, 9, 6, 3, 1];
+
+        let root = create_tree(input);
+        let reverse = create_tree(reversed_input);
+
+        assert_eq!(compare(Solution::invert_tree(root), reverse), true);
+    }
+}
