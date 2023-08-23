b2ab0b58902c5585870e0db9775abed049ff846b
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-23 11:02:00 +0800

    create tree struct to house all tree related functions

--- rust/src/solutions/s026_invert_binary_tree.rs
+++ rust/src/solutions/s026_invert_binary_tree.rs
@@ -44,82 +44,85 @@ impl Solution {
     }
 }
 
-fn create_tree(input: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    let mut root = None;
+pub struct Tree {}
 
-    for &i in input.iter() {
-        root = insert_to_tree(root, i);
-    }
+impl Tree {
+    pub fn create(input: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut root = None;
 
-    root
-}
-
-fn insert_to_tree(
-    mut root: Option<Rc<RefCell<TreeNode>>>,
-    val: i32,
-) -> Option<Rc<RefCell<TreeNode>>> {
-    let node = Some(Rc::new(RefCell::new(TreeNode {
-        val,
-        left: None,
-        right: None,
-    })));
-
-    if root.is_none() {
-        root = node;
-        return root;
-    }
-
-    let mut q = vec![root.to_owned()];
-
-    while !q.is_empty() {
-        let curr = q.remove(0);
-
-        if let Some(n) = curr {
-            if n.borrow().left.is_none() {
-                n.borrow_mut().left = node;
-                return root;
-            } else if n.borrow().right.is_none() {
-                n.borrow_mut().right = node;
-                return root;
-            }
-
-            q.push(n.borrow().left.clone());
-            q.push(n.borrow().right.clone());
+        for &i in input.iter() {
+            root = Tree::insert_to_tree(root, i);
         }
+
+        root
     }
 
-    root
-}
+    fn insert_to_tree(
+        mut root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let node = Some(Rc::new(RefCell::new(TreeNode {
+            val,
+            left: None,
+            right: None,
+        })));
 
-fn compare(a: &Option<Rc<RefCell<TreeNode>>>, b: &Option<Rc<RefCell<TreeNode>>>) -> bool {
-    match (a, b) {
-        (None, None) => true,
-        (Some(_), None) | (None, Some(_)) => false,
-        (Some(a), Some(b)) => {
-            if a.borrow().val != b.borrow().val {
-                return false;
+        if root.is_none() {
+            root = node;
+            return root;
+        }
+
+        let mut q = vec![root.to_owned()];
+
+        while !q.is_empty() {
+            let curr = q.remove(0);
+
+            if let Some(n) = curr {
+                if n.borrow().left.is_none() {
+                    n.borrow_mut().left = node;
+                    return root;
+                } else if n.borrow().right.is_none() {
+                    n.borrow_mut().right = node;
+                    return root;
+                }
+
+                q.push(n.borrow().left.clone());
+                q.push(n.borrow().right.clone());
             }
+        }
 
-            return compare(&a.borrow().left, &b.borrow().left)
-                && compare(&a.borrow().right, &b.borrow().right);
+        root
+    }
+
+    pub fn compare(a: &Option<Rc<RefCell<TreeNode>>>, b: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (a, b) {
+            (None, None) => true,
+            (Some(_), None) | (None, Some(_)) => false,
+            (Some(a), Some(b)) => {
+                if a.borrow().val != b.borrow().val {
+                    return false;
+                }
+
+                return Tree::compare(&a.borrow().left, &b.borrow().left)
+                    && Tree::compare(&a.borrow().right, &b.borrow().right);
+            }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solutions::s026_invert_binary_tree::compare;
-    use crate::solutions::s026_invert_binary_tree::create_tree;
     use crate::solutions::s026_invert_binary_tree::Solution;
+    use crate::solutions::s026_invert_binary_tree::Tree;
 
     #[test]
     fn it_works() {
         let input = vec![4, 2, 7, 1, 3, 6, 9];
         let reversed_input = vec![4, 7, 2, 9, 6, 3, 1];
 
-        let root = Solution::invert_tree(create_tree(input));
-        let reverse = create_tree(reversed_input);
+        let root = Solution::invert_tree(Tree::create(input));
+        let reverse = Tree::create(reversed_input);
 
-        assert_eq!(compare(&root, &reverse), true);
+        assert_eq!(Tree::compare(&root, &reverse), true);
     }
 }
